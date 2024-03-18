9347f20ed9341d4b65dc7e140295ef9690583c45
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-03-19 02:15:58 +0500

    Add files via upload
    I've completed 14th Assighment 🎉 Governer Sindh IT Intiative Kamran Khan Tessori

---
+++ Q14.js
@@ -0,0 +1,6 @@
+"use strict";
+let dinnerInvite = ['WASAY', 'UBAID', 'DANIYAL'];
+let message = "I want to invite you to the night Dinner ";
+for (let index = 0; index < dinnerInvite.length; index++) {
+    console.log(message + dinnerInvite[index]);
+}
---
+++ Q14.ts
@@ -0,0 +1,8 @@
+let dinnerInvite: string[] = [ 'WASAY','UBAID','DANIYAL'];
+let message: string = "I want to invite you to the night Dinner ";
+
+for (let index = 0; index < dinnerInvite.length; index++) {
+    console.log(message + dinnerInvite[index]);
+    
+    
+}
